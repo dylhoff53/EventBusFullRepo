5b0b2214460361958379d14c2baebd4baa51422d
dhoffman548 <112724937+dylhoff53@users.noreply.github.com>
2022-09-16 12:20:20 -0700

    Repo
    wow

---
+++ EventBus/Assets/Scripts/RaceEventType.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.EventBus
+{
+    public enum RaceEventType
+    {
+        COUNTDOWN, START, PAUSE, STOP, FINISH, RESTART, QUIT
+    }
+}
